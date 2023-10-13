3c31a51519001f0e41aba8b2d9461f9a0a460c50
AbMohiz <abmohiz22@gmail.com>
2023-10-14 01:25:13 +0500

    Annotations added manually to api_caller

--- api_caller.py
+++ api_caller.py
@@ -3,7 +3,7 @@ from requests.exceptions import Timeout, ConnectionError
 from decorators import timer
 
 class ApiCaller:
-    def __init__(self, timeout=250):
+    def __init__(self, timeout:int =250) -> None:
         """
         Initializes an ApiCaller object.
 
@@ -13,16 +13,13 @@ class ApiCaller:
         self.timeout = timeout
 
     @timer
-    def get(self, url, params=None):
+    def get(self, url:str) -> requests.Response | None:
         """
         Sends a GET request to the given URL and returns the response object.
-
-        :param url: The URL to send the request to.
-        :param params: Additional parameters to send with the request.
         :return: The response object.
         """
         try:
-            response = self.session.get(url, params=params, timeout=self.timeout)
+            response = self.session.get(url, timeout=self.timeout)
             response.raise_for_status()  # This will raise an HTTPError if the HTTP request returned an unsuccessful status code
             return response
         except Timeout:
