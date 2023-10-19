383bef10ded0e5d0770e3409a70b4d54db27d3a6
faizan <FAIZANNAGHMAN@GMAIL.COM>
2023-10-19 21:44:14 +0500

    update the main.py to extract all subdivisions containing yellowstone and add a short json sample of those properties.
    Signed-off-by: faizan <FAIZANNAGHMAN@GMAIL.COM>

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import json
 
 from models import Property
-from data_extractor import Subdivision, PropertyExtractor
+from data_extractor import PropertyExtractor, CadastralAPI, County
 
-subdivision = Subdivision("YELLOWSTONE ADD", "YELLOWSTONE", "03")
-subdivision.fetch_properties()
+county_list = CadastralAPI.get_counties()  # list of all counties in Montana4
 
-property_extractor = PropertyExtractor(subdivision.properties_html)
-initial_property_data_list = property_extractor.extract_properties()
+madison_county = County("25", "MADISON")
+madison_county.fetch_subdivisions()
+yellow_stone_subdivisions = [subdiv for subdiv in madison_county.subdivisions if 'yellowstone' in subdiv.name.lower()]
 
 complete_property_data = []
 
-for init_data in initial_property_data_list:
-    prop = Property(init_property_data=init_data)
-    complete_property_data.append(prop.data)
+# get all properties in each subdivision
+for subdivision in yellow_stone_subdivisions:
+    subdivision.fetch_properties()
+    property_extractor = PropertyExtractor(subdivision.properties_html)
+    initial_property_data_list = property_extractor.extract_properties()
 
-with open('samples_2.json', 'w') as f:
+    # get all api data for each property from initial data
+    for init_data in initial_property_data_list:
+        prop = Property(init_property_data=init_data)
+        complete_property_data.append(prop.data)
+
+with open('yellowstone_club_properties.json', 'w') as f:
     json.dump(complete_property_data, f, indent=4)
-
