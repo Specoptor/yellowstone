55a6a42e552cc4a8b265d72e7c2034854b52a729
faizan <FAIZANNAGHMAN@GMAIL.COM>
2023-11-04 09:04:59 +0500

    add exception handling and print statements.
    Ensure program does not stop if api call fails.
    
    Signed-off-by: faizan <FAIZANNAGHMAN@GMAIL.COM>

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from models import Property
 from data_extractor import PropertyExtractor, CadastralAPI, County
 from utils import flatten_json_to_df
 
-county_list = CadastralAPI.get_counties()  # list of all counties in Montana4
+county_list = CadastralAPI.get_counties()  # list of all counties in Montana
 
 madison_county = County("25", "MADISON")
 madison_county.fetch_subdivisions()
@@ -16,21 +16,30 @@ complete_property_data = []
 
 # get all properties in each subdivision
 for subdivision in yellow_stone_subdivisions:
-    subdivision.fetch_properties()
-    property_extractor = PropertyExtractor(subdivision.properties_html)
-    initial_property_data_list = property_extractor.extract_properties()
+    initial_property_data_list = []
+    try:
+        subdivision.fetch_properties()
+        property_extractor = PropertyExtractor(subdivision.properties_html)
+        initial_property_data_list = property_extractor.extract_properties()
+        print(f"{len(initial_property_data_list)} properties successfully fetched from {subdivision.name}")
+    except Exception as e:
+        print(e)
 
     # get all api data for each property from initial data
-    for init_data in initial_property_data_list:
-        prop = Property(init_property_data=init_data)
-        complete_property_data.append(prop.data)
+    for idx, init_data in enumerate(initial_property_data_list):
+        try:
+            prop = Property(init_property_data=init_data)
+            complete_property_data.append(prop.data)
+            print(f"{idx+1} out of {len(initial_property_data_list)} properties successfully fetched")
+        except:
+            print("Error with property: ", init_data['Geocode'])
 
 # The path where the flattened JSON will be saved
 output_file_path = 'yellowstone_club_properties.json'
 
 # Flattening the JSON data and converting to a list of dictionaries
 flattened_data = [flatten_json_to_df(property_json)
-                  for i, property_json in enumerate(complete_property_data)]
+                  for i, property_json in enumerate(complete_property_data) if property_json is not None]
 
 # Writing the list of dictionaries to a JSON file
 with open(output_file_path, 'w') as f:
